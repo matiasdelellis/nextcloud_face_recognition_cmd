c38ea5fddd726eefcb36580736cc494d8a51271f
matiasdelellis <mati86dl@gmail.com>
2017-10-01 00:20:07 -0300

    Add known faces folder.. and print Names on json to console..

--- nextcloud-face-detect.py
+++ nextcloud-face-detect.py
@@ -6,52 +6,88 @@ import json
 
 data = {}
 
-def image_find_faces(path):
-    print("Searching faces on: {}".format(path))
-    image = face_recognition.load_image_file(path)
-    face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="hog")
-    faces = len(face_locations)
-    if faces > 0:
-        print("  I found {} face(s) in this photograph.".format(faces))
+def scan_known_people(folder):
+    known_names = []
+    known_face_encodings = []
 
-    for face_location in face_locations:
-        top, right, bottom, left = face_location
-        data['faces-locations'].append({
-            'filemame': path,
-            'top': top,
-            'right': right,
-            'bottom': bottom,
-            'left': left
-        })
+    for filename in os.listdir(folder):
+        fullpath = os.path.join(folder, filename)
+        basename = os.path.splitext(os.path.basename(fullpath))[0]
+        img = face_recognition.load_image_file(fullpath)
+        encodings = face_recognition.face_encodings(img)
 
-def folder_detect_faces (path):
+        if len(encodings) > 1:
+            print ("WARNING: More than one face found in {}. Only considering the first face.".format(filename))
+
+        if len(encodings) == 0:
+            print("WARNING: No faces found in {}. Ignoring file.".format(filename))
+        else:
+            known_names.append(basename)
+            known_face_encodings.append(encodings[0])
+
+    return known_names, known_face_encodings
+
+def image_find_faces(filename, known_encodings, known_names):
+    print("Searching faces on: {}".format(filename))
+    try:
+        image = face_recognition.load_image_file(filename)
+        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="hog")
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+    except:
+         return
+
+    for unknown_encoding in face_encodings:
+        distances = face_recognition.face_distance(known_encodings, unknown_encoding)
+        for distance, name, location in zip(distances, known_names, face_locations):
+            top, right, bottom, left = location
+            print("{},{},{},{},{},{},{}".format(filename, name, distance, top, right, bottom, left))
+            data['faces-locations'].append({
+                'filemame': filename,
+                'name': name,
+                'distance': distance,
+                'top': top,
+                'right': right,
+                'bottom': bottom,
+                'left': left
+            })
+
+def folder_detect_faces (path, known_encodings, known_names):
     for filename in os.listdir(path):
         fullpath = os.path.join(path, filename)
         if os.path.isdir (fullpath):
-            folder_detect_faces (fullpath)
+            folder_detect_faces (fullpath, known_encodings, known_names)
         else:
             if fullpath.lower().endswith(('.png','.jpg','.jpeg')):
-                image_find_faces (fullpath)
+                image_find_faces (fullpath, known_encodings, known_names)
 
-def update_folder(a):
+def analyze_list (args):
     data['faces-locations'] = []
-    folder_detect_faces(a.folder)
-    jsonfile = os.path.join(a.folder, '.faces.json')
-    with open(jsonfile, 'w') as f:
-        json.dump(data, f, indent=4)
+    known_names, known_encodings = scan_known_people(args.known)
 
+    for filename in args.search:
+        if os.path.isdir (filename):
+            folder_detect_faces(filename, known_encodings, known_names)
+        else:
+            if filename.lower().endswith(('.png','.jpg','.jpeg')):
+                image_find_faces (filename, known_encodings, known_names)
+    if True:
+        print (json.dumps(data, indent=2))
+    else:
+        #jsonfile = os.path.join(a.folder, '.faces.json')
+        print('Saving the .faces.json file with the information')
+        with open('.faces.json', 'w') as f:
+            json.dump(data, f, indent=2)
     return 0
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Manage users for PraghaServer')
-    parser.add_argument('operation', nargs='+', choices=['update'], help='Operation')
-    parser.add_argument('--folder', type=str, help='Folder to search faces recursively')
-    parser.add_argument('--to-known', type=str, help='Folder to add face to assign name')
+    parser = argparse.ArgumentParser(description='Analyze pictures in search of faces')
+    parser.add_argument('operation', nargs='+', choices=['analyze'], help='Operation')
+    parser.add_argument('--search', nargs='+', help='List of pictures or folder to search faces')
     parser.add_argument('--known', type=str, help='Folder know face to compare')
 
     args = parser.parse_args()
     op = {
-        'update': update_folder,
+        'analyze': analyze_list,
     }[args.operation[0]]
 
     exit(op(args))
