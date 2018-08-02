1a37981e43e7dc1fd419ed66673b921774f46da4
Matias De lellis <mati86dl@gmail.com>
2018-08-02 18:24:55 -0300

    Don't make comparisons anymore. It only analyzes the photos and returns the faces.

--- nextcloud_face_recognition_cmd/nextcloud_face_recognition_cmd.py
+++ nextcloud_face_recognition_cmd/nextcloud_face_recognition_cmd.py
@@ -23,32 +23,7 @@ def response_append_status(message):
          'message': message
     })
 
-def scan_known_people(folder):
-    known_names = []
-    known_face_encodings = []
-
-    for filename in os.listdir(folder):
-        if filename.startswith('.'):
-            continue
-        fullpath = os.path.join(folder, filename)
-        basename = os.path.splitext(os.path.basename(fullpath))[0]
-
-        img = face_recognition.load_image_file(fullpath)
-        encodings = face_recognition.face_encodings(img)
-
-        if len(encodings) > 1:
-            append_status_response("WARNING: More than one face found in {}. Only considering the first face.".format(filename))
-
-        if len(encodings) == 0:
-            append_status_response("WARNING: No faces found in {}. Ignoring file.".format(filename))
-        else:
-            known_names.append(basename)
-            known_face_encodings.append(encodings[0])
-
-    return known_names, known_face_encodings
-
-
-def image_find_faces(filename, known_encodings, known_names):
+def image_find_faces(filename):
     try:
         image = face_recognition.load_image_file(filename)
         face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="hog")
@@ -63,38 +38,29 @@ def image_find_faces(filename, known_encodings, known_names):
 
     for unknown_encoding, location in zip (face_encodings, face_locations):
         top, right, bottom, left = location
-        distances = face_recognition.face_distance(known_encodings, unknown_encoding)
-        result = list(distances <= 0.6)
-        if True in result:
-            for distance, name in zip(distances, known_names):
-                if distance <= 0.6:
-                    response_append_face(filename, name, 0.0, top, right, bottom, left, unknown_encoding)
-        else:
-            response_append_face(filename, 'unknown', 1.0, top, right, bottom, left, unknown_encoding)
+        response_append_face(filename, 'unknown', 1.0, top, right, bottom, left, unknown_encoding)
 
-def folder_find_faces(path, known_encodings, known_names):
+def folder_find_faces(path):
     if path.startswith('.'):
         return
     for filename in os.listdir(path):
         fullpath = os.path.join(path, filename)
         if os.path.isdir (fullpath):
-            folder_find_faces (fullpath, known_encodings, known_names)
+            folder_find_faces (fullpath)
         else:
             if fullpath.lower().endswith(('.png','.jpg','.jpeg')):
-                image_find_faces (fullpath, known_encodings, known_names)
+                image_find_faces (fullpath)
 
 def analyze_list (args):
     data['status'] = []
     data['faces-locations'] = []
 
-    known_names, known_encodings = scan_known_people(args.known)
-
     for filename in args.search:
         if os.path.isdir (filename):
-            folder_find_faces(filename, known_encodings, known_names)
+            folder_find_faces(filename)
         else:
             if filename.lower().endswith(('.png','.jpg','.jpeg')):
-                image_find_faces (filename, known_encodings, known_names)
+                image_find_faces (filename)
     if True:
         print (json.dumps(data, ensure_ascii=False, indent=2))
     else:
@@ -106,7 +72,6 @@ def main():
     parser = argparse.ArgumentParser(description='Analyze pictures in search of faces')
     parser.add_argument('operation', nargs='+', choices=['analyze'], help='Operation')
     parser.add_argument('--search', nargs='+', help='List of pictures or folders to search faces')
-    parser.add_argument('--known', type=str, help='Folder with knonw face pictures to compare')
 
     args = parser.parse_args()
     op = {
