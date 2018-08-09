2890420f3990e4ae091dfa2b6f91b6426d6cb502
Matias De lellis <mati86dl@gmail.com>
2018-08-09 09:44:50 -0300

    Tiny improve of code using classes and pass model and predicor as argument

--- nextcloud_face_recognition_cmd/nextcloud_face_recognition_cmd.py
+++ nextcloud_face_recognition_cmd/nextcloud_face_recognition_cmd.py
@@ -1,92 +1,97 @@
 #!/usr/bin/env python3
 import argparse
 import os, re
-import face_recognition
 import dlib
+import numpy as np
 import json
 
-data = {}
+class NcFaceRecognition:
+    def __init__(self, predictor, model):
+        self.data = {}
+        self.sp = dlib.shape_predictor(predictor)
+        self.facerec = dlib.face_recognition_model_v1(model)
+        self.detector = dlib.get_frontal_face_detector()
+        self.data['status'] = []
+        self.data['faces-locations'] = []
 
-def response_append_face(filename, name, distance, top, right, bottom, left, encoding=None):
-    data['faces-locations'].append({
-         'filename': filename,
-         'name': name,
-         'distance': distance,
-         'top': top,
-         'right': right,
-         'bottom': bottom,
-         'left': left,
-         'encoding': encoding.tolist() if encoding is not None else None
-    })
+    def appendFace(self, filename, name, distance, top, right, bottom, left, encoding=None):
+        self.data['faces-locations'].append({
+                  'filename': filename,
+                  'name': name,
+                  'distance': distance,
+                  'top': top,
+                  'right': right,
+                  'bottom': bottom,
+                  'left': left,
+                  'encoding': encoding.tolist() if encoding is not None else None
+        })
 
-def response_append_status(message):
-    data['status'].append({
-         'message': message
-    })
+    def appendStatus(self, message):
+        self.data['status'].append({
+                  'message': message
+        })
 
-def image_find_faces(filename):
-    try:
-        image = face_recognition.load_image_file(filename)
-        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="hog")
-        face_encodings = face_recognition.face_encodings(image, face_locations)
-    except:
-        response_append_status("Fail to analyze: {}".format(filename))
-        return
+    def findFaces(self, filename):
+        img = dlib.load_rgb_image(filename)
 
-    if len(face_locations) == 0:
-        response_append_face(filename, 'empty', 0.0, 0, 0, 0, 0)
-        return
+        dets = self.detector(img, 1)
+        if len(dets) == 0:
+            self.appendFace(filename, 'empty', 0.0, 0, 0, 0, 0)
+            return
 
-    for unknown_encoding, location in zip (face_encodings, face_locations):
-        top, right, bottom, left = location
-        response_append_face(filename, 'unknown', 1.0, top, right, bottom, left, unknown_encoding)
+        for k, d in enumerate(dets):
+            shape = self.sp(img, d)
+            face_descriptor = np.array(self.facerec.compute_face_descriptor(img, shape))
+            self.appendFace (filename, "unknown", 1.0, d.top(), d.right(), d.bottom(), d.left(), face_descriptor)
 
-def folder_find_faces(path):
-    if path.startswith('.'):
-        return
-    for filename in os.listdir(path):
-        fullpath = os.path.join(path, filename)
-        if os.path.isdir (fullpath):
-            folder_find_faces (fullpath)
-        else:
-            if fullpath.lower().endswith(('.png','.jpg','.jpeg')):
-                image_find_faces (fullpath)
+    def folderFind(self, path):
+        if path.startswith('.'):
+            return
 
-def analyze_list (args):
-    data['status'] = []
-    data['faces-locations'] = []
+        for filename in os.listdir(path):
+            fullpath = os.path.join(path, filename)
+            if os.path.isdir (fullpath):
+                self.folderFind (fullpath)
+            else:
+                if fullpath.lower().endswith(('.png','.jpg','.jpeg')):
+                    self.findFaces (fullpath)
 
-    for filename in args.search:
+    def analyze(self, filename):
         if os.path.isdir (filename):
-            folder_find_faces(filename)
+            self.folderFind(filename)
         else:
             if filename.lower().endswith(('.png','.jpg','.jpeg')):
-                image_find_faces (filename)
-    if True:
-        print (json.dumps(data, ensure_ascii=False, indent=2))
-    else:
-        with open('.faces.json', 'w') as f:
-            json.dump(data, f, indent=2)
+                self.findFaces (filename)
+
+    def dump(self):
+        print(json.dumps(self.data, ensure_ascii=False, indent=2))
+
+# Main App.
+
+def analyze_list (args):
+    nc = NcFaceRecognition (args.predictor[0], args.model[0])
+    for filename in args.search:
+        nc.analyze(filename)
+    nc.dump()
     return 0
 
 def print_status (args):
     version = dlib.__version__
+    data = {}
     data['dlib-version'] = version
-    data['cuda-support'] = dlib.DLIB_USE_CUDA if version >= '19.9.0' else False
-    data['avx-support'] =  dlib.USE_AVX_INSTRUCTIONS if version >= '19.9.0' else False
-    data['neon-support'] = dlib.USE_NEON_INSTRUCTIONS if version >= '19.9.0' else False
+    data['cuda-support'] = dlib.DLIB_USE_CUDA
+    data['avx-support'] =  dlib.USE_AVX_INSTRUCTIONS
+    data['neon-support'] = dlib.USE_NEON_INSTRUCTIONS
 
-    if True:
-        print (json.dumps(data, ensure_ascii=False, indent=2))
-    else:
-        with open('staus.json', 'w') as f:
-            json.dump(data, f, indent=2)
+    print (json.dumps(data, ensure_ascii=False, indent=2))
+
     return 0
-
 
 def main():
     parser = argparse.ArgumentParser(description='Analyze pictures in search of faces')
     parser.add_argument('operation', nargs='+', choices=['analyze', 'status'], help='Operation')
+    parser.add_argument('--predictor', nargs='+', help='Predictor used to search faces')
+    parser.add_argument('--model', nargs='+', help='Model used to search faces')
     parser.add_argument('--search', nargs='+', help='List of pictures or folders to search faces')
 
     args = parser.parse_args()
